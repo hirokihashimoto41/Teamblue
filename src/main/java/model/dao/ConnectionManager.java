59c15af24ca3967ef18dcb4f525a4b381f5f40c4
T.roki <r-todoroki@seassist.co.jp>
2024-12-12 09:18:19 +0900

    add ConnectionManager

--- src/main/java/model/dao/ConnectionManager.java
+++ src/main/java/model/dao/ConnectionManager.java
@@ -1,10 +1,19 @@
 package model.dao;
-
+ 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
 public class ConnectionManager {
-
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	// URL（jdbcプロトコル:mysqlドライバ:データベースサーバーのホスト名:接続先のデータベース）
+	private final static String URL = "jdbc:mysql://localhost:3306/blueteam_db";
+	//MySQLデータベースに接続するユーザー名
+	private final static String USER = "user_name";
+	//MySQLデータベース接続時のパスワード
+	private final static String PW = "password";
+	public static Connection getConnection() throws SQLException, ClassNotFoundException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		return DriverManager.getConnection(URL, USER, PW);
 	}
-
+ 
 }
